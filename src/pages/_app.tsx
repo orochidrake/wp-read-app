c61a0d2138ec899dc1b88f86ab90ebc5f1e218ea
orochidrake <andrezaguette@gmail.com>
2023-08-07 02:02:40 -0300

    fix-_app-error-any

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 
 import { AppLayout } from "@/components/Layout";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }:AppProps) {
   return (
     <AppContextProvider>
       <AppLayout>
